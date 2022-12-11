fd64ab7ea8d48dc25d554e1f95d034cf94e63fa2
dvalladolid <diego@zennio.com>
2022-12-11 13:05:34 +0100

    Organize folder initialization

---
+++ lua/core/init.lua
@@ -0,0 +1,6 @@
+require("core.impatient")       -- Keep this plugin always the first
+require("core.options")
+require("core.keymaps")
+require("core.autocommands")
+require("core.plugins")
+require("core.nvim-notify")     -- Keep this plugin always the last in the "core" group
