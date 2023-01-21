03cccdeda60dde37a1c55d59a023ead6ce27af4e
dvalladolid <diego@zennio.com>
2023-01-21 18:35:34 +0100

    [illuminate] Add setup options and key mapping

--- lua/user/illuminate.lua
+++ lua/user/illuminate.lua
@@ -1,3 +1,15 @@
+local status_ok, illuminate = pcall(require, "illuminate")
+if not status_ok then
+  return
+end
+
+illuminate.configure({
+  delay = 250,
+})
+
 vim.g.Illuminate_ftblacklist = { 'NvimTree' }
+
+vim.keymap.set("n", "]]", function() require("illuminate").goto_next_reference(true) end, {})
+vim.keymap.set("n", "[[", function() require("illuminate").goto_prev_reference(true) end, {})
 
 -- Used in lsp/handlers.lua
