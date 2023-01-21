03ece4d89c5295ea16d311712866545bcc4ad478
dvalladolid <diego@zennio.com>
2023-01-21 18:15:46 +0100

    [gitsigns] Setup cleanup and improve key mapping

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -6,39 +6,43 @@ end
 local on_attach = function(bufnr)
   local gs = package.loaded.gitsigns
 
-  vim.keymap.set("n", "]c", function()
-      if vim.wo.diff then return ']c' end
-      vim.schedule(function() gs.next_hunk() end)
-      return '<Ignore>'
-    end,
-    { buffer = bufnr, expr = true }
-  );
+  local function map(mode, l, r)
+    vim.keymap.set(mode, l, r, { buffer = bufnr, })
+  end
 
-  vim.keymap.set("n", "[c", function()
-      if vim.wo.diff then return '[c' end
-      vim.schedule(function() gs.prev_hunk() end)
-      return '<Ignore>'
-    end,
-    { buffer = bufnr, expr = true }
-  );
+  map("n", "]h", gs.next_hunk)
+  map("n", "[h", gs.prev_hunk)
+  map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>")
+  map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>")
+  map("n", "<leader>ghS", gs.stage_buffer)
+  map("n", "<leader>ghu", gs.undo_stage_hunk)
+  map("n", "<leader>ghR", gs.reset_buffer)
+  map("n", "<leader>ghp", gs.preview_hunk)
+  map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end)
+  map("n", "<leader>ghd", gs.diffthis)
+  map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
 
-  vim.keymap.set("n", "<leader>gd", gs.diffthis, { });
+  -- vim.keymap.set("n", "]c", function()
+  --     if vim.wo.diff then return ']c' end
+  --     vim.schedule(function() gs.next_hunk() end)
+  --     return '<Ignore>'
+  --   end,
+  --   { buffer = bufnr, expr = true }
+  -- );
+  --
+  -- vim.keymap.set("n", "[c", function()
+  --     if vim.wo.diff then return '[c' end
+  --     vim.schedule(function() gs.prev_hunk() end)
+  --     return '<Ignore>'
+  --   end,
+  --   { buffer = bufnr, expr = true }
+  -- );
+  --
+  -- vim.keymap.set("n", "<leader>gd", gs.diffthis, { });
 end
 
 gitsigns.setup({
-  signs = {
-    add = { text = "▎" },
-    change = { text = "▎" },
-    delete = { text = "▎" },
-    topdelete = { text = "契" },
-    changedelete = { text = "▎" },
-  },
   current_line_blame = true,
-  current_line_blame_opts = {
-  },
   max_file_length = 4000,
   on_attach = on_attach,
 })
-
-vim.keymap.set("n", "<leader>vh", ":Gitsigns preview_hunk_inline<CR>", { silent = true })
-
