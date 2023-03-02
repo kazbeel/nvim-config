ac83ddd0bea0fb2eba0ce3b4178045856f4ab5d3
Diego Valladolid <d.valladolid@factor-eleven.de>
2023-03-02 10:48:37 +0100

    [gitsigns] Remap commands for less typing

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -12,33 +12,14 @@ local on_attach = function(bufnr)
 
   map("n", "]h", gs.next_hunk)
   map("n", "[h", gs.prev_hunk)
-  map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>")
-  map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>")
-  map("n", "<leader>ghS", gs.stage_buffer)
-  map("n", "<leader>ghu", gs.undo_stage_hunk)
-  map("n", "<leader>ghR", gs.reset_buffer)
-  map("n", "<leader>ghp", gs.preview_hunk)
-  map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end)
-  map("n", "<leader>ghd", gs.diffthis)
-  map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
-
-  -- vim.keymap.set("n", "]c", function()
-  --     if vim.wo.diff then return ']c' end
-  --     vim.schedule(function() gs.next_hunk() end)
-  --     return '<Ignore>'
-  --   end,
-  --   { buffer = bufnr, expr = true }
-  -- );
-  --
-  -- vim.keymap.set("n", "[c", function()
-  --     if vim.wo.diff then return '[c' end
-  --     vim.schedule(function() gs.prev_hunk() end)
-  --     return '<Ignore>'
-  --   end,
-  --   { buffer = bufnr, expr = true }
-  -- );
-  --
-  -- vim.keymap.set("n", "<leader>gd", gs.diffthis, { });
+  map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+  map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+  map("n", "<leader>hS", gs.stage_buffer)
+  map("n", "<leader>hu", gs.undo_stage_hunk)
+  map("n", "<leader>hR", gs.reset_buffer)
+  map("n", "<leader>hp", gs.preview_hunk)
+  map("n", "<leader>hb", function() gs.blame_line({ full = true }) end)
+  map("n", "<leader>hd", gs.diffthis)
 end
 
 gitsigns.setup({
