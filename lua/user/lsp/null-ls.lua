49394a24ec0490c350e2b41ab313b7618b04ef45
Diego Valladolid <d.valladolid@factor-eleven.de>
2023-05-03 09:01:48 +0200

    [null-ls] Set configuration file for yamllint

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -8,7 +8,9 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 local sources = {
-	diagnostics.yamllint,
+	diagnostics.yamllint.with({
+		args = { "-c", os.getenv("HOME") .. "/.config/yamllint/config", "--format", "parsable", "-" },
+	}),
 
 	formatting.prettierd,
 	formatting.stylua,
