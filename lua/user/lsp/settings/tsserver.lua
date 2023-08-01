68d25934886d24fafa9eca4ff8cec9ef6af33bc1
Diego Valladolid <d.valladolid@factor-eleven.de>
2023-08-01 06:34:03 +0200

    [lsp] Config import module to "project-relative"

--- lua/user/lsp/settings/tsserver.lua
+++ lua/user/lsp/settings/tsserver.lua
@@ -1,18 +1,21 @@
 local function organize_imports()
-  local params = {
-    command = "_typescript.organizeImports",
-    arguments = { vim.api.nvim_buf_get_name(0) },
-    title = "",
-  }
+	local params = {
+		command = "_typescript.organizeImports",
+		arguments = { vim.api.nvim_buf_get_name(0) },
+		title = "",
+	}
 
-  vim.lsp.buf.execute_command(params)
+	vim.lsp.buf.execute_command(params)
 end
 
 return {
-  commands = {
-    TSLspOrganize = {
-      organize_imports,
-      description = "Organize Imports",
-    },
-  },
+	preferences = {
+		importModuleSpecifierPreference = "project-relative",
+	},
+	commands = {
+		TSLspOrganize = {
+			organize_imports,
+			description = "Organize Imports",
+		},
+	},
 }
