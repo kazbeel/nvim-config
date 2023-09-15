7492669f3b57e11b409bdee11dbad749c3c083e0
Diego Valladolid <d.valladolid@factor-eleven.de>
2023-09-15 19:51:59 +0200

    [notify] Set max height and width

--- lua/user/nvim-notify.lua
+++ lua/user/nvim-notify.lua
@@ -1,11 +1,17 @@
 local status_ok, notify = pcall(require, "notify")
 if not status_ok then
-  return
+	return
 end
 
 notify.setup({
-  stages = "slide",
-  timeout = 3000,
+	stages = "slide",
+	timeout = 3000,
+	max_height = function()
+		return math.floor(vim.o.lines * 0.5)
+	end,
+	max_width = function()
+		return math.floor(vim.o.columsn * 0.5)
+	end,
 })
 
 vim.notify = notify
