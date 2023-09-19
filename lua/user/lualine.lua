4971c12c09e75fa418da8d13bc8d6bc9400a4818
Diego Valladolid <d.valladolid@factor-eleven.de>
2023-09-19 06:57:43 +0200

    [lualine] Add many visual improvements

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -3,6 +3,13 @@ if not status_ok then
 	return
 end
 
+local mode = {
+	"mode",
+	fmt = function(str)
+		return " " .. str
+	end,
+}
+
 local filetype = {
 	"filetype",
 	icon_only = true,
@@ -36,18 +43,32 @@ local location = {
 	padding = { left = 0, right = 1 },
 }
 
+local function lsp_status()
+  if rawget(vim, "lsp") then
+    for _, client in ipairs(vim.lsp.get_active_clients()) do
+      if client.attached_buffers[vim.api.nvim_get_current_buf()] and client.name ~= "null-ls" then
+        return " " .. client.name
+      end
+    end
+  end
+
+  return ""
+end
+
 lualine.setup({
 	options = {
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		globalstatus = true,
 		theme = "auto",
 		disabled_filetypes = { "packer", "NvimTree" },
 	},
 	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch" },
-		lualine_c = { filetype, filename, diagnostics },
-		lualine_x = { diff },
-		lualine_y = { "encoding" },
+		lualine_a = { mode },
+		lualine_b = { "branch", diff },
+		lualine_c = { filetype, filename },
+		lualine_x = { diagnostics },
+		lualine_y = { lsp_status  },
 		lualine_z = { progress, location },
 	},
 	extensions = { "fugitive" },
